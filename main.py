fa1a8cfd03178df08f1fbec95f351269a55b2801
Steve Gossip <stephengossip@gmail.com>
2021-11-11 19:00:44 +0000

    after checking that the price is at an acceptable level, I set up a smtp client session to send an email to myself that tells me the price and gives me the link to buy the product. My next step will be to host on pythonanywhere.com where it will be possible to set my code to run once a day in order to keep checking the price of the prodcut.

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
-from pprint import pprint
+import os
+import smtplib
+
+
 
 BASE_URL = 'https://www.amazon.co.uk/Furbo-Dog-Camera-2-Way-Audio-Compatible/dp/B01FXC7JWQ'
+YAHOO_MAIL_PASS = os.environ.get('YAHOO_MAIL_PASS')
+YAHOO_MAIL_ADD = os.environ.get('YAHOO_MAIL_PASS')
+GMAIL_ADD = 'stephengossip@gmail.com'
 
 HEADERS = {
     'Accept-Language' : 'en-GB,en-US;q=0.9,en;q=0.8',
@@ -15,7 +21,25 @@ furbo = response.text
 
 soup = BeautifulSoup(furbo, 'html.parser')
 price_span = soup.find(name='span', class_='a-offscreen')
-print(price_span.getText())
+price_str = price_span.getText()
+price_float = float(price_str.split("£")[1])
+product_title = soup.find(name='span', id='productTitle').getText().replace('\n','')
+acceptable_price = 195.0
+
+message = f"Subject:Buy Now! \n\nAttention!! \n\n{product_title}\n\n Price is now £{price_float}.\n Buy it now at {BASE_URL}"
+if price_float <= acceptable_price:
+    # try:
+        with smtplib.SMTP("smtp.mail.yahoo.com", 587) as connection:
+            connection.starttls()
+            connection.login(user='appbreweryinfo@yahoo.com', password=YAHOO_MAIL_PASS)
+            connection.sendmail(from_addr='appbreweryinfo@yahoo.com', to_addrs=GMAIL_ADD,
+                                msg=(message.encode()))
+            connection.close()
+
+    # except:
+    #     print('Could not send email')
+
+
 
 
 
