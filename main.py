1fc90507a73ea198feb17d0d598c667943ad9790
Steve Gossip <stephengossip@gmail.com>
2021-11-11 16:40:16 +0000

    Using the url and relevant http header data, I scraped the amazon page for the Furbo dog camera.

---
+++ main.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+from pprint import pprint
+
+BASE_URL = 'https://www.amazon.co.uk/Furbo-Dog-Camera-2-Way-Audio-Compatible/dp/B01FXC7JWQ'
+
+HEADERS = {
+    'Accept-Language' : 'en-GB,en-US;q=0.9,en;q=0.8',
+    'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0'
+                   '.4515.131 Safari/537.36'
+}
+
+response = requests.get(BASE_URL, headers=HEADERS)
+furbo = response.text
+
+soup = BeautifulSoup(furbo, 'html.parser')
+
+
+
